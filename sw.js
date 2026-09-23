5a488f5d5042ea354f8f6e05652cebeddf7cd614
aboalosh86-cyber <aboalosh86@gmail.com>
2026-09-24 00:29:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,22 @@
-const CACHE_NAME='guided-lectures-offline-v4';
-self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./fonts/Cairo-600.ttf','./fonts/Cairo-700.ttf','./fonts/Cairo-800.ttf','./fonts/Cairo-900.ttf','./fonts/Tajawal-500.ttf','./fonts/Tajawal-700.ttf','./fonts/Tajawal-800.ttf']))); });
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy=response.clone(); caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)); return response; }).catch(()=>caches.match('./index.html')))); });
+const CACHE_NAME='guided-lectures-flat-v1';
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll([
+    './index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'
+  ])));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
