511d6c21de8b54ff9bb8ddc54596f9a2784a2b53
aboalosh86-cyber <aboalosh86@gmail.com>
2026-09-24 01:33:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,23 @@
-const CACHE_NAME='guided-lectures-flat-v1';
+const CACHE_NAME='guided-lectures-cairo-icon-v3';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll([
-    './index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'
-  ])));
+  event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)));
 });
 self.addEventListener('activate',event=>{
   event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
       .then(()=>self.clients.claim())
   );
 });
 self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
   event.respondWith(
-    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    fetch(event.request).then(response=>{
       const copy=response.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(event.request,copy));
+      caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)).catch(()=>{});
       return response;
-    }).catch(()=>caches.match('./index.html')))
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
   );
 });
