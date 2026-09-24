a0b5a0a9d7348e264addf5717ff5f59d4524b393
aboalosh86-cyber <aboalosh86@gmail.com>
2026-09-24 12:38:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,20 @@
-const CACHE_NAME='guided-lectures-balanced-images-v3';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+const CACHE_NAME='guided-lectures-offline-landscape-big-v4';
+const FILES=["./","index.html","manifest.webmanifest","icon-180.png","icon-192.png","icon-512.png","Cairo-600.ttf","Cairo-700.ttf","Cairo-800.ttf","Cairo-900.ttf","lecture-01.html","lecture-02.html","lecture-03.html","lecture-04.html","lecture-05.html","lecture-06.html","lecture-07.html","lecture-08.html","lecture-09.html","lecture-10.html","lecture-11.html","lecture-12.html","lecture-13.html","lecture-14.html","lecture-15.html","lecture-16.html","lecture-17.html","lecture-18.html","lecture-19.html","lecture-20.html","lecture-21.html","lecture-22.html","lecture-23.html","lecture-24.html","lecture-25.html","lecture-26.html","lecture-27.html","lecture-28.html","lecture-29.html","lecture-30.html","0031c6c441dd18e27a82.jpg","0051d115f7eb734c6689.jpg","00d42112f66b6ae6af80.jpg","00ffe68a3222ba156f32.jpg","04c4d505397be999d54d.jpg","04c531e6dfa94a2b7af7.jpg","04f61392bd6695f6ec1f.jpg","0535b09bdf1af9012e37.jpg","068b01dc7d58db063563.jpg","0690edede0b896c902d4.jpg","06a8f426b349073f6e3f.jpg","06ecbd155d208425a7f6.jpg","086a16e4af1bdff724fb.jpg","08c83ff666f0393a39cc.png","0931c45f99c10338c51f.jpg","09c1828da72623bd253c.jpg","0a8ae8c2e488cd65a8e5.jpg","0b32caaefd1d5d17dc18.jpg","0b416b7fead05825dbc2.jpg","0b7a4d2d9d070c520d4f.jpg","0bd6e3c072c71af3e1b7.jpg","0c3f6f22a259d91bcbf2.jpg","0c6eea9c33e7edf17e3d.jpg","0c80698769d1eb0dc0f3.jpg","0d3ac9c9607332372c12.jpg","0d6869822847a2bc8b86.jpg","0e57bd09f7d55aa9c295.jpg","0f7f76ab071efea676db.jpg","100cdd5dd33d3a1f591f.png","106d3cbefb522e000665.jpg","1161f76860f385e167d1.jpg","124de91ec3e16d3e95ec.jpg","1270a1d174a264bd3423.png","12ffd5aee687f3fb5006.jpg","13708628f64013a81fdb.jpg","142c20df788752c9c4c2.jpg","14914f8c1f3e658e5010.jpg","1677e52548b6c24720e0.jpg","167853237067588067a0.jpg","16bbc60d33efd31df26d.jpg","179307303b91f8e9110e.jpg","18071d394b2badfb63f3.jpg","1814bd4eb22525493954.jpg","18cc443168c76369b729.jpg","18f84544a8e2e3d044c4.jpg","19088bd1c3055ed51fdc.jpg","196fe2e37b2d3e2b0113.jpg","19efe30620d9632b4555.jpg","1a2d56e7776d491075c4.jpg","1c09bc45339870c1e015.jpg","1c128ac50fa8bab1e725.jpg","1c9456fc5f7726444c3e.jpg","1c9bf594ea1a3b228a53.png","1d8b8fda00907a499ecb.jpg","1e23fe97d36e8fd38fd9.jpg","1f5d8fb42d7ffa75627c.jpg","1fb1e31fc0861060734f.jpg","205ec8cb7402c6c18db3.png","22fa706d34bdb46c4948.jpg","231b3ee6404b6a6bfc91.jpg","23214ad48de4d94ecd12.jpg","233d0ac1b13044d7eec9.jpg","255e8bec61a6333c2fe3.jpg","2627a8cec973559e63f2.jpg","265e05628274d6c3f61b.jpg","280d232b457beaae5568.jpg","285e07a9a27aaadcd49a.jpg","28bb87ad515f8005b03e.jpg","290beb9c683518af678d.jpg","29429533abdfa0255e19.jpg","2962da675dcc168971ef.jpg","29e99673508c59b5a9c2.jpg","2aa5081c025409ce44e5.jpg","2ad657992e822548825e.jpg","2b2e1bc9b9c6e6c4eaf4.jpg","2b72d076c97ae237c1ea.jpg","2bbc8b4a5ae91f9f4937.jpg","2bcc049df619877ccd5b.jpg","2bf9e5f8cae5730c972b.jpg","2cb834fcb85fd3bdb610.jpg","2cc9b1c069bad8607b0e.jpg","2cdcf5f90bba7c9d2325.jpg","2d0651389d808f6fa261.png","2db8fc3513c6a4470d8e.jpg","2efcbf7224f351721a4e.jpg","2f8c4d46728e4e75078a.jpg","304e13fab0089d64bbcd.jpg","306abd4945b2e41a73ce.jpg","3070b3fb43cc57eebd81.jpg","309299de8c0870a8b9af.jpg","30bdac6cee80cc962652.jpg","317abb6e8ac47b85aac1.jpg","3235832c9164db024d8d.jpg","329fbd4ad66106165897.png","32f5396d01c726373cb9.jpg","3420871c989f94188b84.jpg","35b545d260ccb33d919b.png","3608b4f34b7d7c77bfad.jpg","3626508ad9ffe93aa1e7.jpg","362bc205b91a1fad7434.jpg","366737ee1afe024fd529.jpg","372ecdc15ebd77d27228.jpg","3752a2e65d891929ee53.jpg","3874f18f51272aaffc76.jpg","3887bcbdd86543f6d1ac.jpg","3a53ae2d56495b07bec9.jpg","3a94d5a7266a678707cd.jpg","3b94d77163fdb3c1c0d3.jpg","3c11cb6b492a68625f03.jpg","3d03d89ef1402e133619.jpg","3e197e9e22c867825a15.jpg","3ec2014f56a732acf943.jpg","3f82f1a8c44c2e51089f.jpg","40e695230214b0387bf8.jpg","43664d67de1350af7f97.jpg","43776cfd548dc073717f.jpg","43e1b69298e1c385f89c.jpg","449b97b4a9e5d202fe08.jpg","450245c061412df5d95f.jpg","4618ad3d4588150f0b83.jpg","46cd965ae5d65951a6f1.jpg","46fbc2e06e6bee54cba5.jpg","4783d6ca75fb307b92bf.jpg","481e778cfab8bea1f240.jpg","482c1e74ea206ac4b202.jpg","493bdcf5f9415a6ec967.jpg","4940807ce30b303041d6.jpg","4a40214d9c0be62d1ff0.jpg","4a9f7c87c23175e10ee5.png","4b2c57ca824504be2a9d.png","4b350702350668a4cf6a.jpg","4b5e6261055e351b8852.jpg","4b664cee37f91245bfe1.jpg","4c6c942e73a7ec0f3c18.jpg","4d37980522c7825ce3a5.jpg","4d4accc2a55f0e544b14.jpg","4dd14a70b21f21a5a027.jpg","4e4c8b3c5814942c2200.jpg","4e8ddc3241612b31ef7a.jpg","504b9a15d693cbf916dc.jpg","50b84202ab0255cefe5f.jpg","51171e4060e228c539dd.jpg","51c9c6b0be2d6098f482.webp","5304d34112d1de180faa.jpg","534ba985ce8e8ed92220.jpg","5631cfa0b3018ebf5625.jpg","563503647edbcc109b73.jpg","564838dc919e2e615fd7.jpg","56ae8fa3a96db4a91613.jpg","576ebeee5a3c8d115a3d.jpg","576f3304f56d6da334a6.jpg","5791962c37301646e8cd.jpg","57a8a49eb81f661d0c6d.jpg","584b3364031ff52373f6.jpg","586ae2a3e7d89b00633b.jpg","58b4dd98a8c7dea74374.jpg","58daf6609d3434539d5a.jpg","5905ec0ad549d5ba22b7.jpg","59829d3d7bd91fa8029a.jpg","598951b24cfdc751fbf9.jpg","59b13cd60d1e22e09abf.jpg","5a00411f37f4bb5c3040.jpg","5a1ca4cb6fe8433ca5fc.png","5a4df4916fb4eeb05cbf.jpg","5ab0dff6cef5e362d207.jpg","5c88bbb8128648935adb.jpg","5d69d6a193a936346068.jpg","5d7779f22b0cf6c264f5.jpg","5ec4b743908a1f7edc2b.jpg","5ee788f5ca166e4af254.jpg","5ef4a3e8e4d8eb32a4f1.png","5f0e78e3bbae78d22967.png","5f7e03509d49e1fb657c.jpg","5fa80462ad0530b3a7a2.jpg","600cba7183534a6e5e99.jpg","6017b5ddb6bea701226c.jpg","60297f5d7efcd1c20a3e.jpg","602ed5fae4d7998363f2.png","6051f995da411fffe245.jpg","6097df4cc716501026e9.jpg","619b9e615f61c64ea76d.jpg","61a9ae3960233e81b231.jpg","62a52905f75c8595b512.jpg","636fbf37dc7e6310e726.jpg","6473980a694b4c375dd8.jpg","64b8b1baa0f4d9544bed.jpg","650ba6e5351b87ecb4ef.jpg","651f19a39a48334c8ba0.jpg","6579e0d05e8e8b7725cf.jpg","657fd8e2c37923e1aba8.jpg","6587a8927f4092aa643d.jpg","666565d7b37b6837477c.jpg","66baf0a009129c59bbf7.jpg","66da80ce36ad8c7aa507.jpg","670e14a6bdd51462bca9.jpg","677ecdf0bd64ec7b5a96.jpg","67cd3b8ad20e85582180.jpg","67f6d269bc5d4e63158f.jpg","68c6da9f31c275fc9093.jpg","695451e4b2e2282ab99a.jpg","69630c29559c3e713fb1.jpg","6a19eb868b0d8197a051.jpg","6bf2c18d2b6b96748031.jpg","6c1ef026700fc9d0e49b.jpg","6cd75e94451d80ee5acd.jpg","6e590ab52f3d2d45f560.jpg","701ee2975c7c82bc3e9f.jpg","705373a6e281e19ee197.jpg","70be27a40f46284b9fdb.jpg","70c01c68712b90ef7547.jpg","7122ff320a4c6a14c858.jpg","713a9fc990469d17c740.jpg","717f1e71b7f1e6a4a740.jpg","737651d9240a24e270a6.jpg","75730bc4d5196e75122d.jpg","75aa5da5c12e6992bfd0.jpg","763901b32df79baf42b1.jpg","786e23236da12adcd6ca.jpg","78773361c7cd834bba77.jpg","789d5efb010994c70cd5.jpg","78ba4fc476e27cef826b.jpg","78bc895e392ee57dc76e.jpg","7a02ebe938515729f56d.jpg","7a2d7c5459fc00a11884.jpg","7c07c716aee3ba39c5c8.jpg","7c4d05877a7226490f7b.jpg","7d47b1251813078f0bbb.jpg","7e1f75de6d5d5315f1ff.jpg","7ea0386a85e894978558.jpg","7f0a645416a995a65386.jpg","7f17e27b888cb13c3d7d.jpg","7f6bf63505ebb1e1cd94.jpg","7f6d607c124c2e71f1c5.jpg","7f7cfa2d34f3378b884f.jpg","822c1a3f6855f755e321.jpg","8272896878502045d34c.jpg","82c3a0f04dff9f18b362.jpg","834d92aeac8d6c3700ab.png","8359b0e686f13a6ceb1c.jpg","84a9e15186ccdcd2bc1b.png","84c66e022f35569e40a4.jpg","84dfcb6fa3c4438dafd9.jpg","8511749c2bb9cc70ccf3.jpg","852e9df7cfc2c29f0be1.jpg","8679a388f70796f965b6.jpg","86ea1976e467c60400e3.jpg","873d36c0b4c72eb645b8.jpg","87944bfe6d8843372e46.jpg","879c23feea86c78e35c9.jpg","8982bdabb3895fced339.png","8a0b2a0f9496f352d980.jpg","8a2032cf7a38da532806.jpg","8a5114fd3e03f6a1c703.jpg","8ada6bff97ffe4c4c955.jpg","8b84d963337e1b2c48fd.jpg","8befd2d2484367c4cd93.jpg","8bf43608d511b2f15110.jpg","8cb4b0d9830cfb167262.jpg","8cb7c33fd143f8a5d5c5.jpg","8cba004eaac8c868ddf8.jpg","8d4e5004bad45c9040e9.png","8e3e3c69657ea0b778ec.jpg","8e695ff1c2a56ab23e99.jpg","8e7d81a70a0f3e8ebc63.jpg","8e9aa48e8849bea273f3.jpg","8eb28909e3dda79fe4c6.jpg","8eb68818f28fcbf44fbf.jpg","8ef1388dad9846ed6740.jpg","8fce42676a610b23171c.jpg","90ff47471753084ce24b.jpg","91259ff5924611414510.jpg","920f5914ee2171a5c151.jpg","9212d19dd236d807d4e0.jpg","940b02975ac8a8edf5c1.jpg","9440b13e7bb047550871.jpg","94b4253667ba53e45e55.jpg","9572751c3e19aec9ba51.png","957b941d3956624d4427.jpg","95d6651f1d30cc594a47.jpg","9693f8082669811e27e7.jpg","9762a36c825264c9907e.jpg","976e0bcb6b99bf3c2636.jpg","97f87d1539cb954fc362.jpg","982752ceed648e708be6.jpg","982ac642e137de971e13.jpg","98eb0bc763bf1b7fbfa8.jpg","9951006a75f4d37a3c2d.jpg","998e5b0c97498d4cb0ce.jpg","9a47ec5e0dffdb8f7a70.jpg","9c086b4493ad82d64e9d.jpg","9c1eda30794174c61050.jpg","9c296ab7ac2418d9c3fc.jpg","9d154eb14a769b228069.jpg","9d215f896a65949f9537.jpg","9d5c4311d87f42f6d004.jpg","9dd9ccb0ea624967f79f.jpg","9f24245de715dd0a1ac9.jpg","9f259f214d15902c21b0.jpg","9f7109dbf5c9af384761.jpg","9f84a4fc8c01c8ebe4d4.png","9fc64f05b5559dc25e23.jpg","a057a6404504b731fe36.jpg","a09e193bf5b0ebb32c7a.png","a0bbfd197d20fa0dc9c6.jpg","a0db8187f49f77b772ce.png","a14b1dc82b76e681de21.png","a14fa33beb8bd87c0809.jpg","a1806e3e3a34f36adccb.jpg","a5822c041ace17ad8964.jpg","a62341cf36f6bc795f2b.jpg","a77688c4c5a1e3b02bd2.jpg","a92a6869c3ee484f1cf6.jpg","a981f9851cad52924936.jpg","aa8e59d3f9f2feed95a1.jpg","aa9e1b1b50389cbca928.jpg","aaf2a25d4ca88c3ba3fc.jpg","ab30645e6c4ad56d948e.jpg","ae5eafed079bb13699bc.png","aee7856e627e6b337ff7.png","af9c66731b1b77dd9504.jpg","b0821c0c0cdd0a3da658.jpg","b11013c648097ccb95e0.jpg","b170c1831f1f2517f200.jpg","b2a3bf70535f76a5e910.jpg","b2e3c51d52271719cc89.jpg","b39b1d1298959f69ac04.jpg","b39bdbec5379a27c04cf.jpg","b3f8043400c3b401eb13.jpg","b55ae4477afe39cdf0cc.png","b5a8f5197c58d28fb9eb.png","b5ef01e47e590ce9b80f.jpg","b60c861d1d37e311fcd4.jpg","b6e64f971899ece678d2.jpg","b74cb8897a49aea0d16c.jpg","b7c6644e9e883f9897e8.jpg","b85c794d177f425bd268.jpg","b883067af859a178e83d.jpg","bba3d325407db473358d.jpg","bbc02552ef314a0a1ecf.jpg","bcd268284b2ac3e5cbf6.jpg","bd1cd0974f84286e83c6.jpg","bef21e47073bb10348bc.png","bf1143c9da30d1ccf7e8.jpg","bf6d9cc356c7a45686bc.jpg","bfa89332afbb794b744c.jpg","c0667f606b43dc1c5d74.jpg","c0a6e6d0928183d4de34.jpg","c0e7b4b03fd142df654a.jpg","c18a5fb6155d53e95b48.jpg","c2076d3abae8505d08bb.jpg","c2bdaf9dad4db27d15e5.jpg","c2cfd5e1d00adc88b487.jpg","c3644858584f76c61c4a.jpg","c41302ee59f622eae092.jpg","c4169dea9bc7fd3f47e0.jpg","c45dc9cf979a46cdd452.jpg","c604dda11716f4be8ffe.jpg","c6083c4f7b14f6d6aa31.jpg","c610d6284ddd20e6f78a.jpg","c69bad8bc44bf711a2db.jpg","c7262f63dc546d5c9c59.jpg","c73826f6014cbdb18fad.jpg","c83339a9461ca62ae72a.jpg","c89c8e1c122bdcf90528.jpg","c91b9efca8a0dcb3c05a.jpg","c9adff4ade90bc42dd08.jpg","ca31a033575596d8f1de.png","cab062a6d27dafce3cbb.jpg","cc12711e85013a908382.jpg","cc765dc1eeb7de3508f3.png","cca7848c4fba9ef149c5.jpg","cd3b64319c5691e92fa9.jpg","cd75dce1d1041c4ee698.jpg","ceac7ce3defca91d6e7c.jpg","cf3df20e326d525a375c.png","cf4b96166b6c15044e46.jpg","cfecb8e7dd7048db55cc.jpg","d1ede5654a2b628fcecb.jpg","d2460099798a57e4f144.jpg","d3232882c9ca886f0441.png","d3a3a09ccf0ee5ab3b7b.jpg","d453e4fd150ebc71f9de.jpg","d5d6412d29ac94065e9a.jpg","d5e554f296754506216f.jpg","d84f8353b675f7dd2fd5.jpg","d89e8d5186944c2d37e2.jpg","d9164924152956ac5a53.jpg","d91779363742e97bd322.jpg","d92d187470cbfa78df16.jpg","d9eddeabc437bbf5bcb5.jpg","da147be1365ec9894adf.png","db0cff3a53f919f71e27.jpg","db34b8475862d7b45943.jpg","dc333d6c16dbdcb67a5a.jpg","dd5165113560e76b6e5f.jpg","ddcde8ff4d03835b9eac.png","de28dfb6060d02efe758.jpg","de5a1afdafdfae7d0c42.jpg","de7b28b1863a629aa01f.jpg","defe0ec70da64a4dea43.png","df163a7f92de5d22c22e.jpg","df600d4fcde49793c16a.jpg","e008f07ddb600c04f973.jpg","e1488e053ac250b5021d.jpg","e16fad4a9f53c1b00eff.jpg","e2d384c78db5678859ac.jpg","e2f1dce6d958ff3cd82a.jpg","e2f8c0b78b6937b65703.jpg","e4767ecba4db6fb7a8dd.png","e488004cfa5d1464854f.jpg","e4a4e67473bd4fc5e20a.jpg","e4be105157bb8e336fbb.jpg","e5cfa2896ecbdffbcc85.jpg","e5f0a63540437bd183e4.jpg","e64f03b4be7c10d144db.jpg","e6a0fd9c9d677e882f04.jpg","e719244778938aa30932.jpg","e7737653e4b777ea64b9.jpg","e848d44e0fad45c96972.png","e8a1b275fa403ed3bbf5.jpg","e8b68fc04de56b8623f0.jpg","e95328394690982a17d4.jpg","e9c83e777b763c510500.jpg","e9e20ab25a4198ef8108.jpg","ea081575701c8ac20b7a.jpg","ea995dc81147904b12f8.jpg","ead69ec7142ce0c1334b.jpg","eb99e63d6a037cdd31db.jpg","ec58f895e1f11ab81995.jpg","ec5b42f099021bb60edd.jpg","ed2f7c3bf727bc3ab716.jpg","ed3efbe7c10544d38ee8.jpg","eda8eaac5c8ea789c385.jpg","ee2d28f91fecfb85b626.jpg","ee7964801435d0443e36.jpg","ee7e9b760f068d361b1e.jpg","ef6d47e029db6c047a53.jpg","efc67a4b128f1d2caaa0.jpg","f0e4f40e8fd8ef60d399.jpg","f0ede38b666ed6545510.jpg","f168652f57d69119b8b0.jpg","f1ce3b57c25dda539029.jpg","f504e9517f3cd22b9e2c.jpg","f54efb2f82bf661b22d0.jpg","f67ad9e6c87650a297bb.jpg","f7e1b0e3f347af4e3f07.jpg","f8796ac71a16bbca0727.jpg","f8a258b5c53e32f2d6e4.jpg","f90f3705faa07f1e58c5.jpg","f93ad8443dfccba8b82d.jpg","f9c07f5a257296751602.jpg","fb2f217fba1f30c364b9.jpg","fbc8e496d1d4ece528d5.jpg","fc8efad1b7f3a21fa9ae.jpg","fd1317374089ba3aaa21.jpg","fd2c7c023ba651d79453.jpg","fdc347dac2526d14155c.jpg","fe5744615841283d32b4.jpg","fe7e9b61e7a88418af1b.jpg","fe92a75c304c2227f736.jpg","feec9ce57a026bfda2f0.jpg","ff34ac3044ecf783f4b2.jpg","ff7dfe30db3cabdcd357.png"];
+
 self.addEventListener('install',event=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE)).catch(()=>{}));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(async cache=>{
+      for(const url of ['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png']){
+        try{
+          const r=await fetch(url,{cache:'reload'});
+          if(r.ok) await cache.put(url,r);
+        }catch(e){}
+      }
+    })
+  );
 });
+
 self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys()
@@ -11,13 +22,70 @@ self.addEventListener('activate',event=>{
       .then(()=>self.clients.claim())
   );
 });
+
+async function notifyAll(msg){
+  const clients=await self.clients.matchAll({includeUncontrolled:true,type:'window'});
+  for(const c of clients) c.postMessage(msg);
+}
+
+async function cacheAll(){
+  const cache=await caches.open(CACHE_NAME);
+  let done=0, failed=0;
+  const total=FILES.length;
+  for(const url of FILES){
+    try{
+      const hit=await cache.match(url,{ignoreSearch:true});
+      if(!hit){
+        const r=await fetch(url,{cache:'no-store'});
+        if(r.ok) await cache.put(url,r.clone());
+        else failed++;
+      }
+    }catch(e){ failed++; }
+    done++;
+    if(done===1 || done%8===0 || done===total){
+      await notifyAll({type:'CACHE_PROGRESS',done,total});
+    }
+  }
+  if(failed===0) await notifyAll({type:'CACHE_DONE',done,total});
+  else await notifyAll({type:'CACHE_ERROR',done,total,failed});
+}
+
+self.addEventListener('message',event=>{
+  if(event.data && event.data.type==='CACHE_ALL'){
+    event.waitUntil(cacheAll());
+  }
+});
+
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+
+  // HTML/navigation: try fresh online version first, then offline cache.
+  if(event.request.mode==='navigate' || event.request.destination==='document'){
+    event.respondWith(
+      fetch(event.request).then(async r=>{
+        if(r && r.ok){
+          const cache=await caches.open(CACHE_NAME);
+          cache.put(event.request,r.clone()).catch(()=>{});
+        }
+        return r;
+      }).catch(async()=>{
+        const cached=await caches.match(event.request,{ignoreSearch:true});
+        return cached || caches.match('index.html');
+      })
+    );
+    return;
+  }
+
+  // Images/fonts/static files: cache-first for fast classroom use.
   event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)).catch(()=>{});
-      return response;
-    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+    caches.match(event.request,{ignoreSearch:true}).then(cached=>cached || fetch(event.request).then(async r=>{
+      if(r && r.ok){
+        const cache=await caches.open(CACHE_NAME);
+        cache.put(event.request,r.clone()).catch(()=>{});
+      }
+      return r;
+    }))
   );
 });
